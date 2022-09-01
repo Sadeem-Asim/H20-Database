e502d437a98811465423c4698ed7b7765f5d77e9
sadeem <sadeemasim12345@outlook.com>
2022-09-01 20:03:55 +0500

    Cors

--- app.js
+++ app.js
@@ -1,17 +1,47 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 const app = express();
 const { getHashes, newHash } = require("./controllers/hash20Controllers");
 app.use(express.json({ limit: "10kb" }));
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  // res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Content-Security-Policy",
+    "script-src 'self' https://cdnjs.cloudflare.com"
+  );
+  next();
+});
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 // Connecting Database
 mongoose
   .connect(
     "mongodb+srv://hash2o:hash2o@hash2o-instance.upnuf.mongodb.net/?retryWrites=true&w=majority",
     {
-      // useNewUrlParser: true,
-      // useCreateIndex: true,
-      // useFindAndModify: false,
       useUnifiedTopology: true,
     }
   )
